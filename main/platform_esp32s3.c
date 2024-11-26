87325e6e9795334bd2f7dad59bb9c4ded383a4e3
Cristian David Araujo <cristian.araujo@udea.edu.co>
2024-11-25 20:03:25 -0500

    Progress on hardware abstraction for UART on ESP32-S3: added initial functionality; further work needed on read/write functions and macro definitions for sensor registers.

--- main/platform_esp32s3.c
+++ main/platform_esp32s3.c
@@ -10,3 +10,92 @@
  */
 
 #include "platform_esp32s3.h"
+
+/**
+ * @brief Find UART number for the ESP32-S3 microcontroller
+ * 
+ * @param gpio_tx 
+ * @param gpio_rx 
+ * @param gpio_rts 
+ * @param gpio_cts 
+ * @return uart_port_t 
+ */
+static uart_port_t find_uart_num(uint8_t gpio_tx, uint8_t gpio_rx, uint8_t gpio_rts, uint8_t gpio_cts)
+{
+    // UART number for the ESP32-S3 microcontroller
+    if (gpio_tx == 43 && gpio_rx == 44)
+    {
+        return UART_NUM_0;
+    }
+    else
+    {
+        return UART_NUM_1;
+    }
+
+    return UART_NUM_MAX;
+}
+
+bool uart_init(uart_t *uart_config, uint32_t baud_rate, uint16_t buffer_size, uint8_t gpio_tx, uint8_t gpio_rx, uint8_t gpio_rts, uint8_t gpio_cts)
+{
+    // Check if the UART gpio pins are valid
+    if (gpio_tx == UART_PIN_NO_USE || gpio_rx == UART_PIN_NO_USE)
+    {
+        ESP_LOG("UART_INIT", "Invalid UART TX or RX pin");
+        return false;
+    }
+    else if ((gpio_tx != 43 && gpio_rx != 44) || (gpio_tx != 17 && gpio_rx != 18))
+    {
+        ESP_LOG("UART_INIT", "Invalid UART TX or RX pin");
+        return false;
+    }
+
+    
+    // Find UART number for the ESP32-S3 microcontroller
+    uart_port_t uart_num = find_uart_num(gpio_tx, gpio_rx, gpio_rts, gpio_cts);
+
+    // Configure UART parameters
+    uart_config->baud_rate = baud_rate;
+    uart_config->buffer_size = buffer_size;
+    uart_config->uart_num = uart_num;
+    uart_config->gpio_tx = gpio_tx;
+    uart_config->gpio_rx = gpio_rx;
+    uart_config->gpio_rts = gpio_rts;
+    uart_config->gpio_cts = gpio_cts;
+
+    // Configure UART configuration structure
+    uart_config->uart_config.baud_rate = baud_rate;
+    uart_config->uart_config.data_bits = UART_DATA_8_BITS;
+    uart_config->uart_config.parity = UART_PARITY_DISABLE;
+    uart_config->uart_config.stop_bits = UART_STOP_BITS_1;
+    uart_config->uart_config.flow_ctrl = (gpio_rts != UART_PIN_NO_USE && gpio_cts != UART_PIN_NO_USE) 
+                                ? UART_HW_FLOWCTRL_CTS_RTS 
+                                : UART_HW_FLOWCTRL_DISABLE;
+    uart_config->uart_config.source_clk = UART_SCLK_DEFAULT;
+
+    
+
+
+    // Configure UART parameters and check if UART was initialized successfully
+    if (uart_param_config(uart_num, uart_config) != ESP_OK)
+    {
+        ESP_LOG("UART_INIT", "Failed to configure UART parameters");
+        return false;
+    }
+
+    // Configure UART pins
+    if (uart_set_pin(uart_num, gpio_tx, gpio_rx, gpio_rts, gpio_cts) != ESP_OK)
+    {
+        ESP_LOG("UART_INIT", "Failed to configure UART pins");
+        return false;
+    }
+
+    // Install UART driver
+    if (uart_driver_install(uart_num, buffer_size, 0, 0, NULL, 0) != ESP_OK)
+    {
+        ESP_LOG("UART_INIT", "Failed to install UART driver");
+        return false;
+    }
+
+    ESP_LOG("UART_INIT", "UART initialized successfully");
+    return true;
+}
--- main/platform_esp32s3.h
+++ main/platform_esp32s3.h
@@ -12,6 +12,67 @@
 #ifndef PLATFORM_ESP32S3_H
 #define PLATFORM_ESP32S3_H
 
+// Standard C includes
 #include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+// ESP32-S3 specific includes
+#include "driver/uart.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "esp_mac.h"
+
+// Constants for UART configuration
+#define UART_PIN_NO_USE UART_PIN_NO_CHANGE // Do not use pin for UART configuration
+
+// Structure for harware abstraction of UART configuration
+typedef struct {
+    uint32_t baud_rate;         // Baud rate for UART communication
+    uint16_t buffer_size;       // Buffer size for UART communication
+    uart_port_t uart_num;       // UART port number
+    uint8_t gpio_tx;            // GPIO pin for UART TX
+    uint8_t gpio_rx;            // GPIO pin for UART RX
+    uint8_t gpio_rts;           // GPIO pin for UART RTS
+    uint8_t gpio_cts;           // GPIO pin for UART CTS
+    
+    uart_config_t uart_config;  // UART configuration structure
+    uart_event_t uart_event;    // UART event structure
+    uint32_t event_queue_size;  // Event queue size for UART communication
+    QueueHandle_t event_queue;  // Event queue for UART communication
+} uart_t;
+
+/**
+ * @brief Initialize UART peripheral with custom configuration
+ * 
+ * @param uart_config UART configuration structure
+ * @param baud_rate Baud rate for UART communication
+ * @param buffer_size Buffer size for UART communication
+ * @param gpio_tx GPIO pin for UART TX
+ * @param gpio_rx GPIO pin for UART RX
+ * @param gpio_rts GPIO pin for UART RTS
+ * @param gpio_cts GPIO pin for UART CTS
+ * 
+ * @return true if UART initialization was successful
+ * @return false if UART initialization failed
+ */
+bool uart_init(uart_t *uart_config, uint32_t baud_rate, uint16_t buffer_size, uint8_t gpio_tx, uint8_t gpio_rx, uint8_t gpio_rts, uint8_t gpio_cts);
+
+/**
+ * @brief Initialize UART peripheral with default configuration
+ * 
+ * @param uart_t UART configuration structure
+ * @param baud_rate Baud rate for UART communication
+ * @param buffer_size Buffer size for UART communication
+ * @param gpio_tx GPIO pin for UART TX
+ * @param gpio_rx GPIO pin for UART RX
+ * 
+ * @return true if UART initialization was successful
+ * @return false if UART initialization failed
+ */
+bool uart_init_with_defaults(uart_t *uart_config, uint32_t baud_rate, uint16_t buffer_size, uint8_t gpio_tx, uint8_t gpio_rx) {
+    return uart_init(uart_config, baud_rate, buffer_size, gpio_tx, gpio_rx, UART_PIN_NO_USE, UART_PIN_NO_USE);
+}
+
 
 #endif // PLATFORM_ESP32S3_H
